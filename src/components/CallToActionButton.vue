f26eb69671db2e4f9b239c1808021ab50867f38c
Laila Fitriana <laila.fitriana@base22.com>
2021-02-04 18:07:09 -0500

    Created action button component with SVG

---
+++ src/components/CallToActionButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="action-button">
+        <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="145 20, 290 290, 0 290"></polygon>
+        <text x="48%" y="60%" dominant-baseline="middle" text-anchor="middle">UPDATE</text> 
+        <text x="48%" y="70%" dominant-baseline="middle" text-anchor="middle">GOALS</text> 
+      </svg>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'action-button',
+  props: []
+}
+</script>
+
+<style scoped>
+.action-button {
+    background-color: #FCC58E;
+    padding: 3% 0 3% 0;
+    width: 30%;
+    border-radius: 5%;
+}
+.action-button polygon {
+    fill: #42b983;
+    text-align: center;
+}
+.action-button text {
+    font-size: 2em;
+    font-weight: bold; 
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,16 +3,19 @@
         <h1>{{ msg }}</h1>
         <EllipseProgress progress="50" color-line="blue" empty-color-line="#FFFFFF" legend-value="50" legend-value-slot="%" legend-caption="PROGRESS"></EllipseProgress>
         <EllipseProgress progress="25" color-line="blue" empty-color-line="#FFFFFF" legend-value="2" legend-value-slot="/6" legend-caption="ACTIONS"></EllipseProgress>
+        <ActionButton></ActionButton>
     </div>
 </template>
 
 <script>
 import EllipseProgress from '@/components/EllipseProgress.vue';
+import ActionButton from '@/components/CallToActionButton.vue';
 
 export default {
   name: 'dashboard',
   components: {
-    EllipseProgress
+    EllipseProgress,
+    ActionButton
   }
 }
 </script>
