61320e507e9d29daef4da53556f0070b57d217c2
Laila Fitriana <laila.fitriana@base22.com>
2021-02-04 17:19:11 -0500

    Added Ellipse Progress component with props

---
+++ src/components/EllipseProgress.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="ellipse-progress">
+        <vue-ellipse-progress
+          :data="circles"                    
+          :progress="progress"
+          :angle="-90"
+          :color="colorLine"
+          :colorFill="colorFillGradient"
+          :emptyColor="emptyColorLine"
+          :emptyColorFill="emptyColorFillGradient"                      
+          :size="300"
+          :thickness="10"
+          emptyThickness="7%"
+          lineMode="in 10"
+          :legend="true"
+          :legendValue="legendValue"
+          :legendFormatter="({currentValue}) => new Intl.NumberFormat('de-DE').format(currentValue)"
+          legendClass="legend-custom-style"
+          dash="60 0.9"
+          animation="reverse 700 400"
+          :noData="false"
+          :loading="false"                      
+          fontColor="white"
+          :half="false"
+          :gap="10"
+          dot="10 blue"
+          fontSize="5rem">
+          <span slot="legend-value">{{ legendValueSlot }}</span>
+          <p slot="legend-caption">{{legendCaption}}</p>
+        </vue-ellipse-progress>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ellipse-progress',
+  props: [ 'progress', 'colorLine', 'emptyColorLine', 'legendValue', 'legendValueSlot', 'legendCaption']
+}
+</script>
+
+<style scoped>
+.ellipse-progress {
+    background-color: #FCC58E;
+    padding: 3% 0 3% 0;
+    width: 30%;
+    border-radius: 5%;
+}
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="dashboard">
+        <h1>{{ msg }}</h1>
+        <EllipseProgress progress="50" color-line="blue" empty-color-line="#FFFFFF" legend-value="50" legend-value-slot="%" legend-caption="PROGRESS"></EllipseProgress>
+        <EllipseProgress progress="25" color-line="blue" empty-color-line="#FFFFFF" legend-value="2" legend-value-slot="/6" legend-caption="ACTIONS"></EllipseProgress>
+    </div>
+</template>
+
+<script>
+import EllipseProgress from '@/components/EllipseProgress.vue';
+
+export default {
+  name: 'dashboard',
+  components: {
+    EllipseProgress
+  }
+}
+</script>
+
+<style scoped>
+.dashboard {
+  display: flex;
+  justify-content: space-evenly;
+}
+</style>
