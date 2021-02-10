a63ac2c71065ab1354db1c532786a201501ea802
Laila Fitriana <laila.fitriana@base22.com>
2021-02-10 14:07:24 -0500

    Added toggle functionality

--- src/components/CallToActionButton.vue
+++ src/components/CallToActionButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="action-button" v-on:click="updateGoals">
+    <div class="action-button" v-on:click="updateGoals" :path="pathRoute">
         <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg" >
         <polygon points="145 20, 290 290, 0 290"></polygon>
         <text x="48%" y="60%" dominant-baseline="middle" text-anchor="middle">{{ textOne }}</text> 
@@ -11,11 +11,14 @@
 <script>
 export default {
   name: 'action-button',
-  props: ['textOne', 'textTwo'],
+  props: ['textOne', 'textTwo', 'pathRoute', 'className'],
   methods: {
       updateGoals: function(event) {
           if (event) event.preventDefault();
-          this.$router.push({ path: 'goal'})
+
+          if (this.pathRoute !== 'false') {
+              this.$router.push({ path: this.pathRoute})
+          }
       }
   }
 }
@@ -27,6 +30,7 @@ export default {
     padding: 3% 0 3% 0;
     width: 30%;
     border-radius: 5%;
+    cursor: pointer;
 }
 .action-button polygon {
     fill: #42b983;
--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,11 +4,14 @@
       <div class="widget-container">
           <EllipseProgress :progress-data="progressPercentage" color-line="blue" empty-color-line="#FFFFFF" :legend-value="progressPercentage" legend-value-slot="%" legend-caption="PROGRESS"></EllipseProgress>
           <EllipseProgress :progress-data="progressPercentage" color-line="blue" empty-color-line="#FFFFFF" :legend-value="progressAction" :legend-value-slot="progressActionSlot" legend-caption="ACTIONS"></EllipseProgress>
-          <ActionButton text-one="UPDATE" text-two="GOALS"></ActionButton>
-        </div>
-        <div class="goals-container">
+          <ActionButton text-one="UPDATE" text-two="GOALS" path-route="goal"></ActionButton>
+      </div>
+      <div class="widget-container" v-on:click="showGoalDetails">
+        <ActionButton text-one="GOAL" text-two="DETAILS" path-route="false"></ActionButton>
+      </div>
+      <div class="goals-container" v-show="isActive">
           <Goal v-bind:goals="goals"></Goal>
-        </div>
+      </div>
     </div>
 </template>
 
@@ -31,7 +34,8 @@ export default {
       progressPercentage: 0,
       progressAction: 0,
       progressActionSlot: '',
-      name: ''
+      name: '',
+      isActive: false
     }
   },
   methods: {
@@ -49,6 +53,10 @@ export default {
     },
     getPercentage: (data, dataArr) => {
       return dataArr > 0 ? Math.round((data/dataArr) * 100) : 0;
+    },
+    showGoalDetails: function (event){
+      if (event) event.preventDefault();
+      this.isActive = !this.isActive;
     }
   },
   mounted: function() {
@@ -57,7 +65,6 @@ export default {
       .then(submissions => {
         console.log('submissions:' + submissions.data);
         this.submissions = submissions.data;
-        console.log('this.submissions: ' + JSON.stringify(this.submissions));
         this.name = submissions.data[0].data.firstName + ' ' + submissions.data[0].data.lastName;
         this.goals = submissions.data[0].data.editGrid;
         this.progressAction = this.calculateProgress(this.goals);
@@ -77,6 +84,7 @@ export default {
 .dashboard .widget-container {
   display: flex;
   justify-content: space-evenly;
+  margin-bottom: 5%;
 }
 .dashboard .fomio-container {
   margin: 5% 2% 5% 2%;
